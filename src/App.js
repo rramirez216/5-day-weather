dae518088dcd6be67e4faf119ea418d5cfd8a578
Ruben Ramirez <rramirez110911@gmail.com>
2021-04-01 17:21:13 -0700

    initializing repository

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
