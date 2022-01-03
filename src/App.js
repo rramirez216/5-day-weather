0dcbea66a328db71b42bbfdfe489648ae444da7a
Ruben Ramirez <rubenramirez@Rubens-MacBook-Pro.local>
2022-01-02 17:33:41 -0800

    added back ground changing functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components/macro'
 import axios from 'axios'
+
 import GlobalStyles from './GlobalStyles'
 import WeatherCard from './components/WeatherCard'
+
+import Clouds from './assets/Clouds.png'
+import Clear from './assets/Clear.png'
+import Rain from './assets/Rain.png'
+import Overcast from './assets/Overcast.png'
+import Snow from './assets/Snow.png'
+import Thunderstorm from './assets/Thunderstorm.png'
 
 const App = () => {
   const [zip, setZip] = useState('')
   const [weatherData, setWeatherData] = useState(null)
+  const [background, setBackground] = useState(null)
+  const [description, setDescription] = useState(null)
 
   const handleSubmit = () => {
     const fetchData = async () => {
@@ -14,29 +24,50 @@ const App = () => {
         `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
       )
       setWeatherData(res.data)
+      setBackground(res.data.weather[0].main)
+      setDescription(res.data.weather[0].description)
       console.log(res.data)
     }
     fetchData()
   }
 
+  const handleBackground = () => {
+    if (background === 'Clouds') {
+      if (description === 'overcast clouds') {
+        return Overcast
+      }
+      return Clouds
+    } else if (background === 'Rain' && background === 'Drizzle') {
+      return Rain
+    } else if (background === 'Snow') {
+      return Snow
+    } else if (background === 'Thunderstorm') {
+      return Thunderstorm
+    } else {
+      return Clear
+    }
+  }
+
+  console.log(handleBackground())
+
   return (
-    <Wrapper>
+    <Wrapper main={handleBackground()}>
       <div>
-        <label htmlFor='search'>Zip Code:</label>
-        <input
+        <Label htmlFor='search'>Zip Code:</Label>
+        <TextInput
           type='text'
           id='search'
           placeholder='Enter Zip Code'
           onChange={(e) => setZip(e.target.value)}
         />
-        <button
+        <Button
           type='submit'
           onClick={() => {
             handleSubmit()
           }}
         >
           Search
-        </button>
+        </Button>
       </div>
 
       {weatherData && <WeatherCard weatherData={weatherData} />}
@@ -54,7 +85,28 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   gap: 64px;
-  background-image: url('https://images.unsplash.com/photo-1590077428593-a55bb07c4665?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1628&q=80');
+  background-image: url('${({ main }) => main}');
+  background-size: cover;
+  background-repeat: no-repeat;
+`
+const Label = styled.label`
+  color: white;
+  font-weight: 700;
+  font-size: 1.2rem;
+  margin-right: 8px;
+`
+
+const TextInput = styled.input`
+  padding: 8px;
+  border-radius: 4px;
+  border: none;
+  margin-right: 8px;
+`
+const Button = styled.button`
+  background-color: white;
+  padding: 8px 16px;
+  border-radius: 8px;
+  border: none;
 `
 
 export default App
--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -3,30 +3,29 @@ import styled from 'styled-components/macro'
 
 const WeatherCard = ({ weatherData: { main, name, weather } }) => {
   return (
-    <>
-      <Card>
-        <Name>{name}</Name>
-        <Current>{main.temp}&#176;</Current>
-        <p>{weather[0].description}</p>
-        <MinMax>H: {main.temp_max}&#176;</MinMax>
-        <MinMax>L: {main.temp_min}&#176;</MinMax>
-      </Card>
-    </>
+    <Card>
+      <Name>{name}</Name>
+      <Current>{main.temp}&#176;</Current>
+      <p>{weather[0].description}</p>
+      <MinMax>H: {main.temp_max}&#176;</MinMax>
+      <MinMax>L: {main.temp_min}&#176;</MinMax>
+    </Card>
   )
 }
 
 const Card = styled.div`
   width: 200px;
   height: 250px;
-  /* display: flex;
-  flex-flow: column nowrap;
-  align-items: center; */
+  background-color: white;
+  padding: 24px;
+  text-align: center;
+  border-radius: 8px;
 `
 const Name = styled.div`
   font-size: 1.5rem;
 `
 const Current = styled.p`
-  font-size: 2.5rem;
+  font-size: 3rem;
 `
 const MinMax = styled.p`
   display: inline-block;
